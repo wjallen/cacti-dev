8bbb695d657e5ab7eb51759fe1c182d6988260d7
wjallen <wallen@tacc.utexas.edu>
2021-11-08 10:51:57 -0600

    started working on alphafold app tied to mav2 execution system

---
+++ app-dev/alphafold-app/assets/runner.sh
@@ -0,0 +1,20 @@
+# Allow over-ride
+if [ -z "${CONTAINER_IMAGE}" ]
+then
+    version=$(cat ./_util/VERSION)
+    CONTAINER_IMAGE="index.docker.io/library/ubuntu:bionic"
+fi
+. lib/container_exec.sh
+
+
+touch sample.fasta
+echo "> seq from portal" > sample.fasta
+echo ${sequence} > sample.fasta
+
+
+COMMAND="run_alphafold.sh"
+OPTS="--flagfile=$AF2_HOME/test/flags/${PRECISION}_dbs.ff  --fasta_paths=./sample.fasta  --output_dir=./af2_${PRECISION}  --model_preset=${MODEL}"
+
+
+echo "container_exec ${CONTAINER_IMAGE} ${COMMAND} ${OPTS}"
+container_exec ${CONTAINER_IMAGE} ${COMMAND} ${OPTS}
